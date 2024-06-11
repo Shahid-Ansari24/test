6fc9b1f94ae9cee392013e9d19dc9619575e2d11
Shahid <shahidansari2452@gmail.com>
2024-06-11 22:41:55 +0530

    added for loop and string

--- learn.cpp
+++ learn.cpp
@@ -7,6 +7,11 @@ int main() {
   cout << "Again Working with this code" << endl;
   cout << "Adding Another Line of Code" << endl;
   char ch = 'c';
+  string s = "Hello This is STRING";
+
+  for(int i = 0; i < 5; i++) {
+    cout << i << endl;
+  }
 
   return 0;
 }
